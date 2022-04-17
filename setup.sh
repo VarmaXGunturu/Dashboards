7707190057ad212fd91702846d865d64d49e1db2
VarmaXGunturu <r.s.s.gunturu@student.tue.nl>
2022-04-17 14:20:38 +0200

    initial coomit to origin

--- setup.sh
+++ setup.sh
@@ -5,9 +5,20 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
+echo "[theme]
+primaryColor="#172a3a"
+backgroundColor="#172a3a"
+secondaryBackgroundColor="#2bb3a0"
+textColor="#ffffff"
+font="monospace"
+"\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+<<<<<<< HEAD
 " >> ~/.streamlit/config.toml
+=======
+" > ~/.streamlit/config.toml
+
+>>>>>>> 7bf894c (added theme)
