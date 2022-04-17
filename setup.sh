5160fb0ca1019ca6eaaf27605c8e218461535467
VarmaXGunturu <r.s.s.gunturu@student.tue.nl>
2022-04-17 19:00:50 +0200

    fixed error

--- setup.sh
+++ setup.sh
@@ -5,19 +5,10 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "[theme]
-primaryColor="#172a3a"
-backgroundColor="#172a3a"
-secondaryBackgroundColor="#2bb3a0"
-textColor="#ffffff"
-font="monospace"
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-<<<<<<< HEAD
 " >> ~/.streamlit/config.toml
-=======
-" > ~/.streamlit/config.toml
 
->>>>>>> 7bf894c (added theme)
